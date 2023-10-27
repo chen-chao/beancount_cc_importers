657f3979b60289a311028ba958d506145d76556f
Chao Chen <wenbushi@gmail.com>
2023-10-27 14:23:32 +0800

    importer for ms salary

--- README.md
+++ README.md
@@ -9,5 +9,6 @@ Simple importers for my personal usage. Supported sources and formats:
 | 中国农业银行 (ABC) | email |
 | 平安银行 (PingAn)  | email |
 | 微信零钱 (Wechat Lingqian) | csv |
+| Microsoft China salary | json |
 
 Note the repo simply parses ad hoc data sources without relying on official APIs. 
---
+++ tests/test_mssalary.py
@@ -0,0 +1,23 @@
+import os
+import re
+
+from beancount.core.number import ZERO, D
+from beancount.ingest.cache import _FileMemo
+
+from beancount_cc_importers.mssalary import MSSalaryImporter
+   
+def test_mssalary():
+    importer = MSSalaryImporter([('filename', r".*salary_.*\.json")])
+    for test_filename in os.listdir('tests/data'):
+        print(f'Runing test, data: {test_filename}')
+
+        if re.match(r".*salary_.*\.json", test_filename):
+            entries = importer.extract(_FileMemo(os.path.join("tests/data", test_filename)))
+            assert len(entries) >= 1
+
+            num = ZERO
+            for entry in entries:
+                for p in entry.postings:
+                    num += p.units.number
+
+                assert num <= D(0.001)
